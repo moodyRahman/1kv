79dbdc227ef5f8290618b51b0f24b66cdec748aa
Moody Rahman <moody.byte@gmail.com>
2023-04-11 12:32:56 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+    let count = 0
+
+    const click = () => {
+        count += 1;
+    }
+
+    $: {
+        console.log(count)
+    }
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{count}</p>
+<button on:click={click} >click me</button>
+
+<style>
+
+</style>
