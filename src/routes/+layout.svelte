5b54f6f891f3491e4b4afc8bfda97483e3ed6122
Moody Rahman <moody.byte@gmail.com>
2023-04-13 12:00:40 -0400

    global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,19 @@
 <slot></slot>
+
+<style>
+    slot {
+        margin: 100px;
+        padding: 100px;
+    }
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        background-color: #cdcdc2;
+    }
+    :global(html) {
+        box-sizing: border-box;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+</style>
