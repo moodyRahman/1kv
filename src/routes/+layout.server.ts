85af6dd9f3ed6b213e7d1bc6bb6f148e53a20ec0
Moody Rahman <moody.byte@gmail.com>
2023-04-13 11:22:16 -0400

    query all pages of data and make available to the app

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 /** @type {import('./$types').LayoutLoad} */
 export const prerender = true;
 import { STRAPI_KEY } from '$env/static/private';
-
 type Clip = {
 	id: number;
 	attributes: {
@@ -14,8 +13,13 @@ type Clip = {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const load = async (): Promise<{ clips: Clip[] }> => {
+	const clips = [];
+
+	// making the first query to the CMS
+	// also informs us how many more pages of data we have to make a request for
+	// via `data.meta`
 	const res = await fetch(
-		'https://strapi.moodyrahman.com/api/d2-clips?sort=source_creation:desc&pagination[pageSize]=200',
+		'https://strapi.moodyrahman.com/api/d2-clips?sort=source_creation:desc&pagination[pageSize]=25',
 		{
 			headers: {
 				Authorization: `Bearer ${STRAPI_KEY}`
@@ -24,9 +28,34 @@ const load = async (): Promise<{ clips: Clip[] }> => {
 	);
 
 	const data = await res.json();
-	console.log(data.meta);
+	clips.push(...data.data);
+	// if there are additional pages of data we need to query...
+	if (data.meta.pagination.pageCount > 1) {
+		const reqs = [...Array(data.meta.pagination.pageCount).keys()] // generate an array of length "remaining pages of data"
+			.map((x) => x + 2) // transform it so it starts from the second page (strapi starts pages from 1)
+			.map((x) => {
+				// generate the fetch Promise for each page and return them
+				return fetch(
+					`https://strapi.moodyrahman.com/api/d2-clips?sort=source_creation:desc&pagination[pageSize]=25&pagination[page]=${x}`,
+					{
+						headers: {
+							Authorization: `Bearer ${STRAPI_KEY}`
+						}
+					}
+				);
+			});
+
+		const page_data = await Promise.all(reqs); // resolve all the promises
+		const page_json = await Promise.all(page_data.map((x) => x.json()));
+		page_json.forEach(async (x) => {
+			clips.push(...x.data);
+		});
+	}
+
+	console.log(clips.length);
+
 	return {
-		clips: data.data
+		clips: clips
 	};
 };
 
