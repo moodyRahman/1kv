46d8c3bf67b8f98eb2866aac132db024e23f750e
Moody Rahman <moody.byte@gmail.com>
2023-04-15 18:08:56 -0400

    render clip

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,4 +1,17 @@
 
+<script lang="ts">
+    export let data;
+    const clip = data.clips.find(x => x.slug.includes(data.slug))
+</script>
+
+
 <div>
-    hello there
+
+    <video controls width="50%">
+        <source src={`https://moodyrahman.com/files/clips/d2/${data.slug}.mp4`}>
+        <track kind="captions">
+
+    </video>
+
+    {JSON.stringify(clip)}
 </div>
