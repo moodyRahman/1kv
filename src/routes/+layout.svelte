e73fd452414b0aeedbf378a00a46dc0879c0c4ef
Moody Rahman <moody.byte@gmail.com>
2023-04-16 20:34:21 -0400

    animation on page change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,35 @@
-<slot></slot>
+
+<script lang="ts">
+    
+    import {scale} from "svelte/transition"
+
+    export let data;
+</script>
+
+
+{#key data.pathname }
+<div transition:scale>
+    <slot></slot>
+</div>
+{/key}
 
 <style>
     slot {
         margin: 100px;
         padding: 100px;
     }
+
     :global(body) {
-        margin: 0;
-        padding: 0;
-        background-color: #cdcdc2;
+            margin: 0;
+            padding: 60px;
+            background-color: #cdcdc2;
+    }
+    @media screen and (max-width: 600px) {
+        :global(body) {
+            margin: 0;
+            padding: 0;
+            background-color: #cdcdc2;
+        }
     }
     :global(html) {
         box-sizing: border-box;
