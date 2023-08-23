3fc8dcd71dd353fc5b999c7991fab01b78e41075
Moody Rahman <moody.byte@gmail.com>
2023-08-22 23:10:05 -0400

    clip left and right options

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,11 +1,17 @@
 
 <script lang="ts">
+    import { goto } from "$app/navigation";
     export let data;
-    const clip = data.clips.find(x => x.slug.includes(data.slug))
+    const clip_i = data.clips.findIndex(x => x.slug.includes(data.slug))
+    const clip = data.clips[clip_i]
+    const left = data.clips[clip_i-1]
+    const right = data.clips[clip_i+1]
+    const random = data.clips[Math.floor(Math.random()*data.clips.length)];
+
 </script>
 
 
-<div>
+<div class="video">
 
     <video controls width="50%">
         <source src={`https://moodyrahman.com/files/clips/d2/${data.slug}.mp4`}>
@@ -13,5 +19,39 @@
 
     </video>
 
-    {JSON.stringify(clip)}
+    <div>
+        <b>
+            {clip.attributes.title}
+        </b>
+    </div>
+    <div>
+        {clip.attributes.description}
+    </div>
+
+    <div>
+        {#if left}
+        <a href={left.slug}>{"<-"} {left.attributes.title}</a>
+        {/if}
+        
+
+        <a href="/">home</a>
+
+        <a href={random.slug}>random</a>
+
+        {#if right}
+        <a href={right.slug}>{right.attributes.title} {"->"}</a>
+        {/if}
+
+    </div>
+
 </div>
+
+
+<style>
+    .video{
+        display: flex;
+        flex-direction: column; /* Stack elements vertically */
+        justify-content: center; /* Center vertically */
+        align-items: center; /* Center horizontally */
+    }
+</style>
